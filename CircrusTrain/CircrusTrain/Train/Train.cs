69ec07fa7a5e5b005229c0c0788fcfdab4994438
ps220821 <ps220821@edu.summacollege.nl>
2025-03-17 12:57:08 +0100

    Added train,  wagon printer methods

--- CircrusTrain/CircrusTrain/Train/Train.cs
+++ CircrusTrain/CircrusTrain/Train/Train.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CircrusTrain.Interfaces;
+using CircrusTrain.Models;
+using CircrusTrain.Wagons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,72 @@ using System.Threading.Tasks;
 
 namespace CircrusTrain.Train
 {
-    internal class Train
+    public class Train : ITrain
     {
+        private readonly List<Wagon> _wagons = new();
+        private readonly List<Animal> _animals;
+        private readonly IWagonPrinter _printer;
+        private const int MaxSeparatorWagons = 4;
+        private int _separatorWagonCount = 0;
+
+        public IReadOnlyList<Wagon> Wagons => _wagons.AsReadOnly();
+
+        public Train(List<Animal> animals, IWagonPrinter printer)
+        {
+            _animals = new List<Animal>(animals);
+            _printer = printer;
+        }
+
+        public void DistributeAnimals()
+        {
+            SortAnimals();
+            foreach (var animal in _animals)
+            {
+                if (!TryAddToExistingWagon(animal))
+                {
+                    AddToNewWagon(animal);
+                }
+            }
+            _printer.PrintTrain(Wagons);
+        }
+
+        private void SortAnimals()
+        {
+            _animals.Sort((a, b) =>
+            {
+                int dietCompare = b.Diet.CompareTo(a.Diet); // Carnivores first
+                return dietCompare != 0 ? dietCompare : b.Size.CompareTo(a.Size); // Larger first
+            });
+        }
+
+        private bool TryAddToExistingWagon(Animal animal)
+        {
+            foreach (var wagon in _wagons)
+            {
+                if (wagon.CanAddAnimal(animal))
+                {
+                    wagon.AddAnimal(animal);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void AddToNewWagon(Animal animal)
+        {
+            if (animal.Size != Animal.AnimalSize.Large && _separatorWagonCount < MaxSeparatorWagons)
+            {
+                var separatorWagon = new SeparatorWagon();
+                separatorWagon.AddAnimal(animal);
+                _wagons.Add(separatorWagon);
+                _separatorWagonCount++;
+            }
+            else
+            {
+                var wagon = new Wagon();
+                wagon.AddAnimal(animal);
+                _wagons.Add(wagon);
+            }
+        }
     }
 }
---
+++ CircrusTrain/CircrusTrain/Wagons/WagonPrinter.cs
@@ -0,0 +1,32 @@
+﻿using CircrusTrain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircrusTrain.Wagons
+{
+    public class WagonPrinter : IWagonPrinter
+    {
+        public void PrintWagon(IWagon wagon)
+        {
+            string wagonType = wagon is SeparatorWagon ? "Separator Wagon" : "Wagon";
+            Console.WriteLine($"{wagonType} with {wagon.Animals.Count} animals (Points: {wagon.CurrentPoints}/10)");
+            foreach (var animal in wagon.Animals)
+            {
+                Console.WriteLine($" - {animal.Name} ({animal.Size}, {animal.Diet})");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintTrain(IReadOnlyList<IWagon> wagons)
+        {
+            Console.WriteLine($"Train with {wagons.Count} wagons:");
+            foreach (var wagon in wagons)
+            {
+                PrintWagon(wagon);
+            }
+        }
+    }
+}
